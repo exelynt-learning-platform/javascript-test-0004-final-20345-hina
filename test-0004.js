91d673d135b8de385a6ce03bc33a69e833f1123b
Shaikh Hina <106679795+Shaikhhina@users.noreply.github.com>
2026-03-14 15:05:31 +0530

    Create test-0004.js

---
+++ test-0004.js
@@ -0,0 +1,15 @@
+let n = 4;
+let size = 2 * n - 1;
+
+for (let i = 0; i < size; i++) {
+    let row = "";
+    for (let j = 0; j < size; j++) {
+        let top = i;
+        let left = j;
+        let right = size - 1 - j;
+        let bottom = size - 1 - i;
+        let min = Math.min(top, left, right, bottom);
+        row += (n - min) + " ";
+    }
+    console.log(row);
+}
